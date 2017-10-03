2842a0a14d1b7a2ea8c7fd44dd022a95772351de
naikanand93 <anand.naik@columbia.edu>
2017-10-02 22:03:57 -0400

    Save method changed

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,8 +6,12 @@ def hello():
     return render_template("input_form.html")
 
 
-@app.route("/save")
+@app.route("/save", methods=['GET', 'POST'])
 def saveData():
-    return "Hello World!"
+	data = request.get_data()
+	print(data)
+	#save data to db
+	return "Hello World!"
+	#return data & id generated in db.
 
 
